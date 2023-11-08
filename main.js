254800655ecfb4e92f227de8fa8655956b5a98ed
Thiyash05 <sandyramar55@gmail.com>
2023-11-08 11:34:05 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,21 @@
+// Output:
+// Flyer runs at 20 mph.
+
+// use bind method
+
+
+let runner = {
+    name: 'Runner',
+    run: function(speed) {
+        console.log(this.name + ' runs at ' + speed + ' mph.');
+    }
+};
+
+let flyer = {
+    name: 'Flyer',
+    fly: function(speed) {
+        console.log(this.name + ' flies at ' + speed + ' mph.');
+    }
+};
+
+ runner.run.apply(flyer,[20])
